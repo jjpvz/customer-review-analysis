f509434b0d4d2b63a72072c3b203abc99924c440
jjpvz <Julianvanzwol0@gmail.com>
2022-01-26 14:02:57 +0100

    Add generateSoup function

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.bol.com/nl/nl/'
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+
+def generateSoup():
+    url = 'https://www.bol.com/nl/nl/p/de-jongen-de-mol-de-vos-en-het-paard/9200000128095686/?promo=main_803_POPC_B3_product_0_&bltgh=m0R10Q-aPf-OK5J1ISlMyw.49_gYnngoQ1VrqwYEo3071Khg_0_1_2.3.ProductImage'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
+
